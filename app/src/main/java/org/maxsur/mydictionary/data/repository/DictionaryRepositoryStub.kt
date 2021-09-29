0e17652e373351e475f30419aa55ac40e09423ba
Maxim Surovtsev <realitybelieves@gmail.com>
2021-09-29 15:42:26 +0300

    Stub repository implementation

---
+++ app/src/main/java/org/maxsur/mydictionary/data/repository/DictionaryRepositoryStub.kt
@@ -0,0 +1,29 @@
+package org.maxsur.mydictionary.data.repository
+
+import io.reactivex.Single
+import org.maxsur.mydictionary.domain.model.Language.EN
+import org.maxsur.mydictionary.domain.model.Language.RU
+import org.maxsur.mydictionary.domain.model.Translation
+import org.maxsur.mydictionary.domain.model.Word
+import org.maxsur.mydictionary.domain.repository.DictionaryRepository
+
+class DictionaryRepositoryStub : DictionaryRepository {
+
+    private val words = listOf(
+        Word("dog", "собака", Translation(EN, RU)),
+        Word("house", "дом", Translation(EN, RU))
+    ).toMutableList()
+
+    override fun getWords(search: String?): Single<List<Word>> {
+        return Single.just(words)
+    }
+
+    override fun translate(word: String, translation: Translation): Single<Word> {
+        return Single.just(Word(word, "translated", translation))
+    }
+
+    override fun saveWord(word: Word): Single<Word> {
+        words.add(word)
+        return Single.just(word)
+    }
+}
